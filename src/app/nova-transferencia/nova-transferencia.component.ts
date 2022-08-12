38ee28fa29bf1337b73f533854efcba9e6d52d58
silviomuniz17 <silvio.carlos.17@hotmail.com>
2022-08-11 23:27:42 -0300

    obs

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovaTransferenciaComponent implements OnInit {
 
-   ngOnInit(): void { }
-  transferir(){
+  ngOnInit(): void {
+  }
+
+  transferir() {
     console.log('Solicitada nova transferência');
   }
 
