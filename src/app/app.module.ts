8e1e9e11a0d6e6c433c98c3dee3d5551232d1157
silviomuniz17 <silvio.carlos.17@hotmail.com>
2022-08-15 21:42:13 -0300

    Exportanto dados do componente aula 07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponent
+    NovaTransferenciaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
