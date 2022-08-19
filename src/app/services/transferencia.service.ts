56fab5e2d0dfd5ac59cf0592fef8e6d70737ebca
silviomuniz17 <silvio.carlos.17@hotmail.com>
2022-08-18 21:35:42 -0300

    06-Comunicação - Atividade 01

---
+++ src/app/services/transferencia.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransferenciaService {
+
+  private listatransferencia: any[];
+
+  constructor() {
+    this.listatransferencia = [];
+  }
+
+  get transferencias(){
+    return this.listatransferencia;
+  }
+
+  adicionar(transferencia: any){
+    this.hidratar(transferencia)
+
+    this.listatransferencia.push(transferencia);
+  }
+
+  private hidratar(transferencia: any){
+    transferencia.data = new Date();
+  }
+
+}
